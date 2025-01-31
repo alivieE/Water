909a1c62503240027e7093ae744a5bd66572799a
MaksimRomanovvv <109852966+MaxRomanov5@users.noreply.github.com>
2025-01-31 13:08:47 +0200

    1

--- aquatrack/src/components/Calendar/Calendar.jsx
+++ aquatrack/src/components/Calendar/Calendar.jsx
@@ -25,7 +25,7 @@ const Calendar = ({ setPickedDate, waterActions }) => {
     console.log(percent);
     return { fullDate: date, percentDay: percent > 100 ? 100 : percent };
   });
-  console.log(422);
+  console.log(4222);
   return (
     <ul className={s.calendarList}>
       {calculateDates.map((day) => {
